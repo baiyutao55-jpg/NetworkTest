3718b6f7f680f328c1ca5e9ee61ec4f3c3db43db
baiyutao <baiyutao55@gmail.com>
2026-01-21 15:26:16 +0800

    networktest commit

---
+++ app/src/main/java/com/example/networktest/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.networktest
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sendRequestBtn=findViewById<Button>(R.id.sendRequestBtn)
+        sendRequestBtn.setOnClickListener {
+            sendRequestWithOkHttp()
+        }
+    }
+
+    private fun sendRequestWithOkHttp(){
+        thread{
+            try {
+                val client= OkHttpClient()
+                val request=Request.Builder()
+                    .url("https://news.163.com")
+                    .build()
+                val response=client.newCall(request).execute()
+                val responsedata= response.body.string()
+                showResponse(responsedata)
+
+            }
+            catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+private fun showResponse(response:String){
+
+    Log.d("Okhttp","reponse:$response")
+    runOnUiThread {
+        val responseText=findViewById<TextView>(R.id.responseText)
+        responseText.text=response
+    }
+}
+
+
+
+}
